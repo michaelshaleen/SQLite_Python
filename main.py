298fdeb791b41614a1083bee0b60595cfd2d5b03
michaelshaleen <shaleenmichael@yahoo.com>
2022-10-10 10:29:59 -0500

    some sql

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import sqlite3
 
 connection = sqlite3.connect("gta.db")
+cursor = connection.cursor()
+
+cursor.execute("create table gta (release_year integer, release_name text, city text)")
+
+
+
 
 release_list = [
     (1997, "Grand Theft Auto", "state of New Guernsey"),
@@ -11,5 +17,18 @@ release_list = [
     (2008, "Grand Theft Auto IV", "Liberty City"),
     (2013, "Grand Theft Auto V", "Los Santos")
 ]    
+
+cursor.executemany("insert into gta values (?, ?, ?)", release_list)
+
+
+for row in cursor.execute("select * from gta"):
+  print(row)
+
+print("*********************")
+cursor.execute("select * from gta where city=:c", {"c": "Liberty City"})
+gta_search = cursor.fetchall()
+print(gta_search)
+
+
 
 connection.close()
