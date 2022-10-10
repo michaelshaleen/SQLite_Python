40604e5e9d117d778747d4ed36c9ce1f8f305330
michaelshaleen <shaleenmichael@yahoo.com>
2022-10-10 10:20:27 -0500

    main py added

---
+++ main.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+connection = sqlite3.connect("gta.db")
+
+release_list = [
+    (1997, "Grand Theft Auto", "state of New Guernsey"),
+    (1999, "Grand Theft Auto 2", "Anywhere, USA"),
+    (2001, "Grand Theft Auto III", "Liberty City"),
+    (2002, "Grand Theft Auto: Vice City", "Vice City"),
+    (2004, "Grand Theft Auto: San Andreas", "state of San Andreas"),
+    (2008, "Grand Theft Auto IV", "Liberty City"),
+    (2013, "Grand Theft Auto V", "Los Santos")
+]    
+
+connection.close()
