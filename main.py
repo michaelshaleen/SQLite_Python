80fdc63e72d89807c1eb541bdb5218460e143ffb
michaelshaleen <shaleenmichael@yahoo.com>
2022-10-10 10:44:07 -0500

    db manipulation via sqltie

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ connection = sqlite3.connect("gta.db")
 cursor = connection.cursor()
 
 cursor.execute("create table gta (release_year integer, release_name text, city text)")
-
-
-
-
 release_list = [
     (1997, "Grand Theft Auto", "state of New Guernsey"),
     (1999, "Grand Theft Auto 2", "Anywhere, USA"),
@@ -25,10 +21,20 @@ for row in cursor.execute("select * from gta"):
   print(row)
 
 print("*********************")
+
 cursor.execute("select * from gta where city=:c", {"c": "Liberty City"})
 gta_search = cursor.fetchall()
 print(gta_search)
+print("*********************")
 
+cursor.execute("create table cities (gta_city text, real_city text)")
+cursor.execute("insert into cities values (?, ?)", ("Liberty City", "New York"))
+cursor.execute("select * from cities where gta_city=:c", {"c": "Liberty City"})
+cities_search = cursor.fetchall()
+print(cities_search)
 
-
+print("*********************")
+for i in gta_search:
+  adjusted = [cities_search[0][1] if value == cities_search[0][0] else value for value in i]
+  print(adjusted)
 connection.close()
